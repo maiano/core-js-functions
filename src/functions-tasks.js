bfa3aa47307312f7e4da2833710bf99b26dde414
maiano <maiano@yandex.ru>
2025-03-28 08:34:05 +0100

    feat: 11 test pass

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -225,8 +225,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  return () => {
+    const id = count;
+    count += 1;
+    return id;
+  };
 }
 
 module.exports = {
